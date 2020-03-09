5146355a48eef19b108035cb46cbb49858761e68
vitorsantos <vitorsantos@afa75b66-6afd-49bc-916c-1ecdd8b3ecb5>
2020-03-09 12:58:00 +0000

    [update]Adicionando o campo CST IPI no xml das notas de saída.
    git-svn-id: https://svn.autogeral.com.br/repositorio/NfeIntegracao4@1458 afa75b66-6afd-49bc-916c-1ecdd8b3ecb5

--- src/br/com/jcomputacao/nfeEmissor/IntegracaoNfe.java
+++ src/br/com/jcomputacao/nfeEmissor/IntegracaoNfe.java
@@ -1324,7 +1324,7 @@ public class IntegracaoNfe extends Servico {
         Imposto imp = new Imposto();
 
         imp.getContent().add(new ObjectFactory().createTNFeInfNFeDetImpostoICMS(icms(item)));        
-        if (this.tributaIpi && !simples) {
+        if (!simples) {
             imp.getContent().add(new ObjectFactory().createTNFeInfNFeDetImpostoIPI(ipi(item)));
         }
         imp.getContent().add(new ObjectFactory().createTNFeInfNFeDetImpostoPIS(pis(item)));
@@ -2362,7 +2362,7 @@ public class IntegracaoNfe extends Servico {
             } else {
                 IPINT ipint = new IPINT();
                 ipi.setIPINT(ipint);
-                ipint.setCST("53");
+                ipint.setCST("99");
             }
         }
         if(this.tributaIpi && item.getIpiSt() == 55) {
