9d78b50ccb969d50194df80bb5fe007b37de44d6
murilo.lima <murilo.lima@host.docker.internal>
2021-02-18 12:44:29 -0300

    [fix] arrumando erro com logica para cst 51

--- src/br/com/jcomputacao/nfeEmissor/IntegracaoNfe.java
+++ src/br/com/jcomputacao/nfeEmissor/IntegracaoNfe.java
@@ -1469,7 +1469,7 @@ public class IntegracaoNfe extends Servico {
                     tributacaoIcms51.setCST(st);
                     tributacaoIcms51.setOrig(origem);
                     icms.setICMS51(tributacaoIcms51);
-                } if("10".equals(st)) {
+                } else if("10".equals(st)) {
                     atribuiIcms10(icms, item, origem, st);
                 } else {
                     atribuiIcms00(icms, item, origem, st);
